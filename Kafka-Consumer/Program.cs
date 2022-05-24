27239944c5c60f9c1812e448b8c0d6824398842f
Sidney Sebastião da Costa Junior <s.da.costa.junior@avanade.com>
2022-05-24 15:44:00 -0300

    first commit

---
+++ Kafka-Consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using Confluent.Kafka;
+
+var conf = new ConsumerConfig
+{
+    GroupId = "test-consumer-group",
+    BootstrapServers = "127.0.0.1:9092",
+    AutoOffsetReset = AutoOffsetReset.Latest
+};
+
+using var c = new ConsumerBuilder<Ignore, string>(conf).Build();
+
+c.Subscribe("test-topic");
+
+var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_, e) =>
+{
+    e.Cancel = true;
+    cts.Cancel();
+};
+
+try
+{
+    while (true)
+    {
+        try
+        {
+            var cr = c.Consume(cts.Token);
+            Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+        }
+        catch (ConsumeException e)
+        {
+            Console.WriteLine($"Error occured: {e.Error.Reason}");
+        }
+    }
+}
+catch (OperationCanceledException)
+{
+    c.Close();
+}
---
+++ Kafka-Producer/Program.cs
@@ -0,0 +1,23 @@
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+using var p = new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+    var count = 0;
+    while (true)
+    {
+        var dr = await p.ProduceAsync("test-topic",
+            new Message<Null, string> { Value = $"test: {count++}" });
+
+        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset} | {count}'");
+
+        Thread.Sleep(2000);
+    }
+}
+catch (ProduceException<Null, string> e)
+{
+    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+}
